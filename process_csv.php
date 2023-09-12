27d0fb1cc09945f475b2be1d78b48bde4fe67185
Sunil Singh <sunilsingh2019@gmail.com>
2023-09-12 20:05:42 +1000

    change on script

--- process_csv.php
+++ process_csv.php
@@ -1,50 +1,81 @@
+#!/usr/bin/php
 <?php
-// process_csv.php
-$mysqli = new mysqli("database", "myuser", "mypassword", "mydb");
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+// Ensure that the script is run from the command line
+if (php_sapi_name() !== 'cli') {
+    exit("This script can only be run from the command line.\n");
 }
 
-if (($handle = fopen($argv[1], "r")) !== FALSE) {
-    $stmt = $mysqli->prepare("INSERT INTO users (name, surname, email) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $name, $surname, $email);
+// Check if a CSV file argument is provided
+if ($argc < 2) {
+    exit("Usage: php user_upload.php users.csv\n");
+}
 
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $name = ucfirst(strtolower($data[0])); // Capitalize first letter, make lowercase
-        $surname = ucfirst(strtolower($data[1])); // Capitalize first letter, make lowercase
-        $email = strtolower($data[2]); // Make lowercase
+$inputFile = $argv[1];
 
-        // Check if the email is valid using regular expression
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match('/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/', $email)) {
-            echo "Invalid email: $email\n";
-            continue; // Skip the current iteration
-        }
+// Check if the input file exists
+if (!file_exists($inputFile)) {
+    exit("Input file not found.\n");
+}
 
-        // Check for duplicate emails before inserting
-        $check_duplicate = $mysqli->prepare("SELECT id FROM users WHERE email = ?");
-        $check_duplicate->bind_param("s", $email);
-        $check_duplicate->execute();
-        $check_duplicate->store_result();
+// Create a PDO connection to the MySQL database
+try {
+    $db = new PDO('mysql:host=database;dbname=mydb', 'myuser', 'mypassword');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    exit("Database connection failed: " . $e->getMessage() . "\n");
+}
 
-        if ($check_duplicate->num_rows > 0) {
-            echo "Error inserting record: Duplicate entry for email '$email'\n";
-            $check_duplicate->close();
-            continue; // Skip the current iteration
-        }
+// Create the users table if it doesn't exist
+$db->exec("CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    email VARCHAR(255) UNIQUE
+)");
 
-        $check_duplicate->close();
+// Read and process the CSV file
+$handle = fopen($inputFile, 'r');
+if ($handle === false) {
+    exit("Error opening input file.\n");
+}
 
-        // Insert the record
-        if (!$stmt->execute()) {
-            echo "Error inserting record: " . $stmt->error . "\n";
-        }
+$invalidEmails = [];
+$insertedRecords = 0;
+
+while (($data = fgetcsv($handle)) !== false) {
+    $name = ucfirst(strtolower(trim($data[0])));
+    $surname = ucfirst(strtolower(trim($data[1])));
+    $email = strtolower(trim($data[2]));
+
+    // Validate the email format using filter_var
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $invalidEmails[] = $email;
+        continue; // Skip this record
     }
-    fclose($handle);
-    $stmt->close();
-} else {
-    echo "Error opening file\n";
+
+    // Insert the record into the database
+    try {
+        $stmt = $db->prepare("INSERT INTO users (name, surname, email) VALUES (:name, :surname, :email)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':surname', $surname);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $insertedRecords++;
+    } catch (PDOException $e) {
+        // Handle duplicate entry error or other database errors here
+        // You can log the error or display a message as needed
+    }
 }
 
-$mysqli->close();
+fclose($handle);
+
+// Report invalid email addresses and the number of records inserted
+if (!empty($invalidEmails)) {
+    echo "Invalid email addresses:\n";
+    foreach ($invalidEmails as $invalidEmail) {
+        echo "Invalid email: $invalidEmail\n";
+    }
+}
+
+echo "Inserted $insertedRecords records into the database.\n";
 ?>
